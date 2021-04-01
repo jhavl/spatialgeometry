0aad19991c34dfd3e069ea87b4fc50c10af99b6e
jhavl <jhavl@users.noreply.github.com>
2021-04-02 08:25:08 +1000

    added supressor

--- spatialgeometry/__init__.py
+++ spatialgeometry/__init__.py
@@ -1,5 +1,6 @@
 from spatialgeometry.Shape import Shape
 from spatialgeometry.CollisionShape import CollisionShape, Mesh, Cylinder, Box, Sphere
+from spatialgeometry.tools import *
 
 __all__ = [
     'Shape',
