ee974f2b252025ffddabcad42743df9c98f9ab34
Jesse Haviland <jhavl@users.noreply.github.com>
2021-04-21 12:39:43 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="spatialgeometry",
-    version="0.1.1",
+    version="0.1.2",
     description="A Shape and Geometry Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
