12657b622f6235aa470267e798611ba9a181b773
jhavl <jhavl@users.noreply.github.com>
2022-02-08 14:01:22 +1000

    made a list

--- spatialgeometry/SceneGroup.py
+++ spatialgeometry/SceneGroup.py
@@ -6,6 +6,7 @@
 from numpy import ndarray, eye, zeros, copy as npcopy
 from spatialmath.base import r2q
 from abc import ABC
+from collections import UserList
 
 # from roboticstoolbox.robot.ETS import ETS
 from typing import Type
@@ -13,9 +14,16 @@ from typing import Type
 from spatialgeometry.SceneNode import SceneNode
 
 
-class SceneGroup(SceneNode):
+class SceneGroup(SceneNode, UserList):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def __getitem__(self, i: int):
+        return self._scene_children[i]
+
+    @property
+    def data(self):
+        return self._scene_children
 
     def hello(self):
         print("HEllo")
