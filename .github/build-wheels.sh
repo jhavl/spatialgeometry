7e6c2cbf81c0719e0cf7b26c6ebacacf2a639649
Jesse Haviland <jhavl@users.noreply.github.com>
2022-12-06 15:21:37 +1000

    Update build-wheels.sh

--- .github/build-wheels.sh
+++ .github/build-wheels.sh
@@ -6,19 +6,17 @@ function repair_wheel {
     if ! auditwheel show "$wheel"; then
         echo "Skipping non-platform wheel $wheel"
     else
-        auditwheel repair "$wheel" --plat "$PLAT" -w /io/wheelhouse/
+        auditwheel repair "$wheel" --plat "$PLAT" -w /io/build/
     fi
 }
 
 cd ./io
 
-# yum install gfortran libopenblas-dev liblapack-dev
-
 # Compile wheels
 # cp3[1,7-9][-,0] == cp37-cp37 to cp310-cp310
 for PYBIN in /opt/python/cp3[1,7-9][-,0]*/bin; do
     "${PYBIN}/pip" install numpy==1.17.4
-    "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
+    "${PYBIN}/pip" wheel /io/ --no-deps -w build/
 done
 
 # Bundle external shared libraries into the wheels
@@ -26,10 +24,4 @@ for whl in wheelhouse/*.whl; do
     repair_wheel "$whl"
 done
 
-# Install packages and test
-#for PYBIN in /opt/python/cp3[6-8]*/bin/; do
-#    "${PYBIN}/pip" install roboticstoolbox-python[dev,collision,vpython] --no-index -f /io/wheelhouse
-#    ("${PYBIN}/pytest")
-#done
-
-ls ./wheelhouse
+ls ./build
